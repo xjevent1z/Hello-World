246753fd9c529cdef40294dfcc3a3a6820a862fc
xjevent1z <qq3025566@gmail.com>
2020-01-19 00:26:04 +0800

    hello world

--- Hello-World.py
+++ Hello-World.py
@@ -1,1 +1,29 @@
-print 'Hello World'
+from datetime import datetime
+"""
+```python
+    >>> agent = HelloWorld()
+    >>> print(agent)
+```
+"""
+__author__ = 'David Chou'
+__copyright__ = "Copyright {}, David Chou".format(datetime.now().year)
+__credits__ = ['David Chou']
+__license__ = 'GPL'
+__version__ = '1.0.0'
+__maintainer__ = "David Chou"
+__email__ = "qq3025566@gmail.com"
+__status__ = "Production"  #  Prototype, Development, or "Production"
+
+
+class HelloWorld:
+    string = str()
+
+    def __init__(self):
+        self.string = 'Hello World!'
+    
+    def __str__(self):
+        return self.string
+
+if __name__ == "__main__":
+    agent = HelloWorld()
+    print(agent)
